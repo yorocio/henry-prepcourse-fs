cdb94bc45b1ff2d4c2e7aa764ddbd5711e632495
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2023-09-26 11:29:59 -0300

    crearUsuario

--- M07 JavaScript Clases/Ejercicio 01.js
+++ M07 JavaScript Clases/Ejercicio 01.js
@@ -10,6 +10,18 @@ function crearUsuario() {
    // El valor {{nombre}} debe ser el nombre definido para la instancia de la clase.
    // Retornar la clase.
    // Tu código:
+   class Usuario {
+      constructor(usuario, nombre, email, password) {
+         this.usuario = usuario;
+         this.nombre = nombre;
+         this.email = email;
+         this.password = password;
+      }
+         saludar () {
+            return 'Hola, mi nombre es ' + this.nombre;
+         }
+      }
+   return Usuario;
 }
 
 function agregarMetodoPrototype(Usuario) {
