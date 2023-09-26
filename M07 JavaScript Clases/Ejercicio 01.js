d326f378b81fc8443f4d02bea43c25da3283ecfe
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2023-09-26 11:47:15 -0300

    agregarStringInvertida

--- M07 JavaScript Clases/Ejercicio 01.js
+++ M07 JavaScript Clases/Ejercicio 01.js
@@ -37,6 +37,9 @@ function agregarStringInvertida() {
    // Agrega un método al prototipo de "String" que devuelva la misma cadena de caracteres, pero invertida.
    // El método debe llamarse "reverse".
    // [PISTA]: necesitarás utilizar el objeto "this".
+   String.prototype.reverse = function (){
+      return this.split ('').reverse().join('');
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
