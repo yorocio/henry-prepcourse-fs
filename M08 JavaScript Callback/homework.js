7a6adb0057f2102097b4d40a3de53ac263ba3a84
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2023-09-27 09:04:11 -0300

    operacionMatematica

--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -20,6 +20,7 @@ function operacionMatematica(num1, num2, cb) {
    // El callback realiza una operación matemática, por lo que necesita de los dos números.
    // Retorna el resultado del callback pasándole como parámetros los números.
    // Tu código:
+   return cb (num1, num2);
 }
 
 function sumarArray(arrayOfNumbers, cb) {
