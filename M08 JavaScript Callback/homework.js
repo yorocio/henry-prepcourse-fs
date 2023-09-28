9c5f67c74a2a434bcd732947d3184c4e1cf9e202
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2023-09-28 09:06:24 -0300

    filter

--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -62,6 +62,13 @@ function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
+   let newArray = [];
+  for (let i = 0; i <arrayOfStrings.length; i++){
+   if (arrayOfStrings[i].charAt(0) === 'a'){
+      newArray.push(arrayOfStrings[i]);
+   }
+  }
+  return newArray;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
